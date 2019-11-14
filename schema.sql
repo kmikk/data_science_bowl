7df232d8975a96080331a244edca8fd182abb73f
jlangree <biglangree@gmail.com>
2019-11-13 19:29:46 -0600

    added raw weather data csv, updated merged csvs, added sql file, ETL notebook

---
+++ schema.sql
@@ -0,0 +1,43 @@
+DROP TABLE IF EXISTS weather;
+DROP TABLE IF EXISTS solar_ogilvie;
+DROP TABLE IF EXISTS solar_minneapolis;
+
+
+
+CREATE TABLE weather (
+    weather_date_time TIMESTAMP,
+    weather_description VARCHAR,
+    clouds_all INT,
+    temp_f FLOAT,
+    pressure INT,
+    humidity INT,
+    wind_speed INT,
+    wind_deg INT,
+    rain_1h INT,
+    snow_1h INT,
+    weather_main VARCHAR,
+    CONSTRAINT pk_weather PRIMARY KEY (
+        weather_date_time
+     )
+);
+
+CREATE TABLE solar_ogilvie (
+    og_date_time TIMESTAMP,
+    power_delivered INT,
+    energy_delivered INT,
+    cumulative_energy INT,
+    CONSTRAINT pk_solar_ogilvie PRIMARY KEY (
+        og_date_time
+     )
+);
+
+CREATE TABLE solar_minneapolis (
+    mpls_date_time TIMESTAMP,
+    power_delivered INT,
+    energy_delivered INT,
+    cumulative_energy INT,
+    CONSTRAINT pk_solar_minneapolis PRIMARY KEY (
+        mpls_date_time
+     )
+);
+
