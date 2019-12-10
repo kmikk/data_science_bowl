cf94edf41ddf4925686bc80e412a79cdfb59fbd7
Andy Lindberg <andrewgeorgelindberg@gmail.com>
2019-12-10 07:17:39 -0600

    fixed olg to avoid AWS bill

--- js/olgnn.js
+++ js/olgnn.js
@@ -1,7 +1,7 @@
 // Plotly.d3.csv("https://45.56.66.135/static/csv/mpls_nn.csv", function (row1) {
 //     Plotly.d3.csv("https://45.56.66.135/static/csv/mpls_xg.csv", function (row2) {
-        Plotly.d3.csv("../resources/olg_nn.csv", function (row3) {
-            Plotly.d3.csv("../resources/olg_xg.csv", function (row4) {
+        Plotly.d3.csv("solar_machine_learning/resources/olg_nn.csv", function (row3) {
+            Plotly.d3.csv("solar_machine_learning/resources/olg_xg.csv", function (row4) {
                 // Plotly.d3.csv("CMH01_2017-2018_TrollData.csv", function (row5) {
                     // Plotly.d3.csv("CGL01_2017-2018_TrollData.csv", function (row6) {
 
